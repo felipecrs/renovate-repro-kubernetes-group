d0836e664211b1e3a810efb792e6f98013eeb2da
Felipe Santos <felipecassiors@gmail.com>
2026-03-03 18:36:42 -0300

    Depend on client-go

--- main.go
+++ main.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/yaml"
 )
+
+func newClientset() (*kubernetes.Clientset, error) {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, nil).ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
+}
 
 func main() {
 	pod := corev1.Pod{}
